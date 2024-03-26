bbdd4518d74a900f77490acaee3dab967543a15b
kimhyonseong <corea6524@gmail.com>
2024-03-27 00:14:44 +0900

    Feat: 링크드리스트 기반 연결리스트 ADT

---
+++ src/main/dataStructure/List/DLinkedList.java
@@ -0,0 +1,54 @@
+package main.dataStructure.List;
+
+public class DLinkedList {
+  /**
+   * 초기화
+   */
+  public DLinkedList() {}
+
+  /**
+   * 리스트에 데이터 저장
+   */
+  public void listInsert(LData data) {
+
+  }
+
+  /**
+   * 첫번째 데이터 참조
+   * @return
+   */
+  public boolean listFirst() {
+    return true;
+  }
+
+  /**
+   * 다음 데이터 참조
+   * @return
+   */
+  public boolean listNext() {
+    return true;
+  }
+
+  /**
+   * 마지막 데이터 삭제, 반환
+   * @return
+   */
+  public LData listRemove() {
+    return null;
+  }
+
+  /**
+   * 리스트에 저장되어 있는 데이터 수 반환
+   * @return
+   */
+  public int listCount() {
+    return 0;
+  }
+
+  /**
+   * 리스트에 정렬의 기준이 되는 함수 등록
+   */
+  public void setSortRule() {
+
+  }
+}
